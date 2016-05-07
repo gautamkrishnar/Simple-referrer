41e9c4501eb13574a171ce2c9a8a864aee046706
Gautam krishna.R <rgautamkrishna@gmail.com>
2016-05-07 18:00:13 +0530

    fixed bugs

--- saveref.php
+++ saveref.php
@@ -1,7 +1,8 @@
 <?php
-$rfrr ="\n".$_GET['ref'];
-if($_GET['ref'])
+$rfrr = date("h:i:sa")." ".date("d-m-Y")." : ".$_POST['ref'];
+echo $rfrr;
+if($_POST['ref'])
 {
-file_put_contents("referrer.txt", rfrr . "\r\n", FILE_APPEND);
+file_put_contents("referrer.txt", $rfrr . "\r\n", FILE_APPEND);
 }
 ?>
